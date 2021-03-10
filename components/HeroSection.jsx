caa34ea3e1c907f24c3cc91065f45dd5a33139e9
Ike <ikechang@beeinventor.com>
2021-03-10 16:31:14 +0800

    Section to component

---
+++ components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function HeroSection() {
+    return <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                {/* <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" id="product">Product</h1> */}
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-light text-gray-900">
+                    DasIoT
+      </h1>
+                <p className="mb-8 leading-relaxed">DasIoT is a all in one IoT platform that help you to manage construction site with custom IoT device integration.</p>
+                <span><a className="underline hover:text-gray-900" href="#contact">Contact us</a> for more detail.</span>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                {/* <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" /> */}
+                <img className="object-cover object-center rounded" alt="hero" src="arch.png" />
+            </div>
+        </div>
+    </section>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import HeroSection from '../components/HeroSection'
+import ContactSection from '../components/ContactSection'
 
 export default function Home() {
   return (
@@ -7,45 +9,11 @@ export default function Home() {
       <Header />
       <main>
         <h1 id="product" className="title-font sm:text-4xl text-3xl pt-8 mb-4 font-medium text-gray-900">Product</h1>
-        <section className="text-gray-600 body-font">
-          <div className="container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              {/* <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" id="product">Product</h1> */}
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-light text-gray-900">
-                DasIoT
-              </h1>
-              <p className="mb-8 leading-relaxed">DasIoT is a all in one IoT platform that help you to manage construction site with custom IoT device integration.</p>
-              <span><a className="underline hover:text-gray-900" href="#contact">Contact us</a> for more detail.</span>
-            </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              {/* <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" /> */}
-              <img className="object-cover object-center rounded" alt="hero" src="arch.png" />
-            </div>
-          </div>
-        </section>
+        <HeroSection/>
 
         <h1 id="contact" className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Contact</h1>
-        <section className="text-gray-600 body-font relative mb-8">
-          <div className="absolute inset-0 bg-gray-300">
-            <iframe width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" scrolling="no" style={{ filter: `grayscale(0.5) contrast(1.2) opacity(0.4)`, pointerEvents: 'none' }} src="https://maps.google.com/maps?ll=25.069082, 121.535371&amp;width=100%25&amp;height=600&amp;hl=en&amp;q=10452%E5%8F%B0%E5%8C%97%E5%B8%82%E4%B8%AD%E5%B1%B1%E5%8D%80%E7%8E%89%E9%96%80%E8%A1%971%E8%99%9F+(DasIot)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-          </div>
-          <div className="container px-5 py-24 mx-auto flex">
-            <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact us with email</h2>
-              {/* <p className="leading-relaxed mb-5 text-gray-600">We will reach out to you shortly</p> */}
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email*</label>
-                <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message*</label>
-                <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-              </div>
-              <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Submit</button>
-              <p className="text-xs text-gray-500 mt-3">We will reach out to you shortly.</p>
-            </div>
-          </div>
-        </section>
+        <ContactSection/>
+        
       </main>
       <Footer />
     </div>
