8ec71ce179c074ccd4625afcda875f437e9ee6a4
Ike <ikechang@beeinventor.com>
2021-03-11 16:55:18 +0800

    Add cesium map

--- components/MapView.jsx
+++ components/MapView.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Viewer, Entity, CameraFlyToBoundingSphere } from "resium";
-import { Cartesian3, Math, BoundingSphere, HeadingPitchRange } from "cesium";
-// import Cesium from "cesium";
-// Cesium.Ion.defaultAccessToken = "YOUR_ACCESS_TOKEN";
+import { Viewer, Scene, Globe, CameraFlyToBoundingSphere, Primitive,Cesium3DTileset } from "resium";
+import { Cartesian3, Math, BoundingSphere, HeadingPitchRange, IonResource, CesiumTerrainProvider } from "cesium";
+import * as Cesium from 'cesium';
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxYjA3ZTUxZi1kNzM3LTQ1NWMtYmZlZS01NjExOThlNzY4ZTYiLCJpZCI6NDU3MzIsImlhdCI6MTYxNTQ1MTAzM30.W3KyreYaVN-XAUsImLy-XX9_ShU88EpfKs_u5EfCp9U";
 
 export default function MapView() {
+    const ionTerrainProvider = IonResource.fromAssetId(1)
+    const terrainProvider = new CesiumTerrainProvider({url: ionTerrainProvider});
+
     return <Viewer
+        terrainProvider={terrainProvider}
+        className="pb-4"
         homeButton={false}
         navigationHelpButton={false}
+        animation={false}
         timeline={false}
         fullscreenButton={false}
         infoBox={false}
@@ -16,8 +22,15 @@ export default function MapView() {
         scene3DOnly
     >
         <CameraFlyToBoundingSphere
-            boundingSphere={new BoundingSphere(Cartesian3.fromDegrees(140, 35.7, 0), 0)}
-            offset={new HeadingPitchRange(0, Math.toRadians(-30), 80000)}
-            duration={3} />
+            boundingSphere={new BoundingSphere(Cartesian3.fromDegrees(121.550767, 25.049728, 0), 0)}
+            offset={new HeadingPitchRange(0, Math.toRadians(-30), 400)}
+            duration={4} />
+
+        <Scene>
+            <Globe depthTestAgainstTerrain={false}></Globe>
+            <Primitive>
+                <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
+            </Primitive>
+        </Scene>
     </Viewer>
 }
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,18 @@ import 'cesium/Build/Cesium/Widgets/widgets.css'
 export default function Home() {
   const MapView = dynamic(
     () => import('../components/MapView'),
-    { ssr: false }
+    {
+      ssr: false,
+      loading: () => <div className="flex justify-center items-center h-48">
+        <span>
+          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-300" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path className="opacity-15" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </span>
+        <h2> Dashboard example is loading... </h2>
+      </div>
+    }
   )
 
   return (
@@ -20,12 +31,12 @@ export default function Home() {
       <Header />
       <main>
         <h1 id="product" className="title-font sm:text-4xl text-3xl pt-8 mb-4 font-medium text-gray-900">Product</h1>
-        <HeroSection/>
+        <HeroSection />
         <MapView/>
 
         <h1 id="contact" className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Contact</h1>
-        <ContactSection/>
-        
+        <ContactSection />
+
       </main>
       <Footer />
     </div>
